1589e94d94d88e902aa0f41e101a5dab234378a9
ROHIT12-bit <reddyrohit823@gmail.com>
2025-11-17 08:31:46 +0530

    idk

--- plugins/force_subs.py
+++ plugins/force_subs.py
@@ -12,32 +12,52 @@ async def not_subscribed(_, client, message):
         user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id)
         if user.status == enums.ChatMemberStatus.BANNED:
             return True
-        else:
-            return False
+        return False
     except UserNotParticipant:
-        pass
-    return True
+        return True
+
 
 @Client.on_message(filters.private & filters.create(not_subscribed))
 async def forces_sub(client, message):
+
     buttons = [
-        [InlineKeyboardButton(text="•ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ•", url=f"https://t.me/botskingdoms")]
+        [
+            InlineKeyboardButton(
+                text="• ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ •",
+                url=f"https://t.me/{Config.FORCE_SUB}"
+            )
+        ]
     ]
-    text = "<b>Yᴏᴜ Bᴀᴋᴋᴀᴀ...!! \n<blockquote>Jᴏɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴜsᴇ ᴍʏ\n\nᴏᴛʜᴇʀᴡɪsᴇ Yᴏᴜ ᴀʀᴇ ɪɴ ʙɪɢ sʜɪᴛ...!!</blockquote>\n<blockquote></b>"
+
+    text = (
+        "<b>Yᴏᴜ Bᴀᴋᴋᴀᴀ...!!</b>\n\n"
+        "<blockquote>Jᴏɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴜsᴇ ᴍʏ ʙᴏᴛ.\n"
+        "ᴏᴛʜᴇʀᴡɪsᴇ Yᴏᴜ ᴀʀᴇ ɪɴ ʙɪɢ sʜɪᴛ...!!</blockquote>"
+    )
+
     try:
         user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id)
+
         if user.status == enums.ChatMemberStatus.BANNED:
-            return await client.send_message(message.from_user.id, text="Sorry You Are Banned To Use Me")
-        buttons.append([InlineKeyboardButton(text="Cʟɪᴄᴋ ʜᴇʀᴇ", url=f"https://t.me/bot_kingdoms_auto_renamerbot?start=true")])
+            return await message.reply_text("Sorry, you are banned from using this bot.")
+
+        buttons.append([
+            InlineKeyboardButton(
+                text="Cʟɪᴄᴋ ʜᴇʀᴇ",
+                url="https://t.me/bot_kingdoms_auto_renamerbot?start=true"
+            )
+        ])
+
     except UserNotParticipant:
+
         if Config.FSUB_PIC:
-            await message.reply_photo(
-                Config.FSUB_PIC,
+            return await message.reply_photo(
+                photo=Config.FSUB_PIC,
                 caption=text,
                 reply_markup=InlineKeyboardMarkup(buttons)
             )
-        else:
-            await message.reply_text(
-                f"Yᴏᴜ ʙᴀᴋᴋᴀᴀ...!! Jᴏɪɴ ɪᴛ...",
-                reply_markup=InlineKeyboardMarkup(buttons)
-            )
+
+        return await message.reply_text(
+            "Yᴏᴜ Bᴀᴋᴋᴀᴀ...!!\nJᴏɪɴ ᴘʟᴇᴀsᴇ...",
+            reply_markup=InlineKeyboardMarkup(buttons)
+        )
